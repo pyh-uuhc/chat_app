a7123ecea39f566fe1a3f61a6b53141516cde841
youngha <pyh.uuhc@gmail.com>
2024-07-24 18:21:06 +0900

    add main.go for client

--- client/main.go
+++ client/main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Error connecting to server:", err)
+		return
+	}
+	defer conn.Close()
+
+	go readMessage(conn)
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		message, _ := reader.ReadString('\n')
+		fmt.Fprint(conn, message)
+	}
+}
+
+func readMessage(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Disconnected from server")
+			return
+		}
+		fmt.Printf(message)
+	}
+}
